d60620396760884315d0b01e462f02108da2e84a
1twalters01 <1twalters01@gmail.com>
2023-04-28 23:09:54 +0100

    ssr with hydration

--- Back-end/Spyrals-FastAPI/Spyrals/settings.py
+++ Back-end/Spyrals-FastAPI/Spyrals/settings.py
@@ -1,6 +1,7 @@
 
 ORIGINS = [
     "http://127.0.0.1:8080",
+    "http://localhost:3000",
 ]
 
 MIDDLEWARE_SETTINGS={
--- Front-end/spyrals-leptos/src/app.rs
+++ Front-end/spyrals-leptos/src/app.rs
@@ -1,6 +1,51 @@
 use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context(cx);
+
+    view! {
+        cx,
+
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes>
+                    <Route path="" view=|cx| view! { cx, <HomePage/> }/>
+                    <Route path="/fetch" view=|cx| view! { cx, <Fetch/> }/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage(cx: Scope) -> impl IntoView {
+    // Creates a reactive value to update the button
+    let (count, set_count) = create_signal(cx, 0);
+    let on_click = move |_| set_count.update(|count| *count += 1);
+
+    view! { cx,
+        <h1>"Welcome to Leptos!"</h1>
+        <button on:click=on_click>"Click Me: " {count}</button>
+    }
+}
+
+
+
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Msg {
@@ -38,7 +83,7 @@ async fn fetch_msgs(_count: u32) -> Result<Vec<String>, FetchError> {
 
 
 #[component]
-pub fn FetchExample(cx: Scope) -> impl IntoView {
+pub fn Fetch(cx: Scope) -> impl IntoView {
     let (count, _set_count) = create_signal::<u32>(cx, 0);
     let msgs = create_local_resource(cx, count, fetch_msgs);
 
